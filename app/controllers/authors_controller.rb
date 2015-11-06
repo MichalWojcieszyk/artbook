fff82ec352044d8d52e420b9b41fdf59cea174b4
Michał Wojcieszyk <mwojcieszyk@gmail.com>
2015-11-06 16:16:28 +0100

    author creation proces

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,9 @@
 class AuthorsController < ApplicationController
+  expose(:author_category)
+  expose(:author)
+  expose(:authors) {Author.order(:rating)}
+  expose(:author_comments, ancestor: :author)
+
   def index
   end
 
@@ -12,11 +17,33 @@ class AuthorsController < ApplicationController
   end
 
   def create
+    self.author = Author.new(author_params)
+    author.user_id = current_user.id
+    if author.save
+      authors << author
+      redirect_to authors_path, notice: "Author was successfully created"
+    else
+      render :new
+    end
   end
 
   def update
+    if author.update(author_params)
+      redirect_to authors_path, notice: "Author was successfully updated"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    if author.destroy
+      redirect_to authots_path
+    end
+  end
+
+  private
+
+  def author_params
+    params.require(:author).permit(:name, :birth_date, :die_date, :hometown, :photo, :biography)
   end
 end
